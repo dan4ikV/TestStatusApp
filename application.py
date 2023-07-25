cd36420c3f346f047b709e5d65d7e73a9e24f3e8
danylo-vasylyshyn <danylo.vasylyshyn@vitechteam.com>
2023-07-25 07:57:30 -0500

    Upload the app

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@application.route('/status')
+def status():
+    return {'status': 'OK', 'code': 200}
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run()
